e06a738c4976471621ad9c4aeaca4a5d7f430240
chaut1936 <chautran0513@gmail.com>
2025-04-19 11:20:34 -0500

    Confirm borrow media page change

--- client/src/components/media/MediaBorrowConfirmation.js
+++ client/src/components/media/MediaBorrowConfirmation.js
@@ -10,8 +10,8 @@ const MediaBorrowConfirmation = ({ media, userData, actionType, handleAction, na
       <p>Are you sure you want to {actionType.toLowerCase()} this media?</p>
       <div className="media-details">
         <p><strong>Type:</strong> {media.Type}</p>
-        <p><strong>Model:</strong> {media.Title}</p>
-        <p><strong>Brand:</strong> {media.Author}</p>
+        <p><strong>Title:</strong> {media.Title}</p>
+        <p><strong>Author:</strong> {media.Author}</p>
         <p><strong>Borrow Duration:</strong> {borrowDays} days</p>
       </div>
       <div className="button-group">
